9706bae25a8c10610091b85680e953a64a57dce3
鲁德镐 <937074603@qq.com>
2023-03-23 19:01:23 +0800

    finished turtial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
-   }
+  },
+  integrations: [preact()]
 });
